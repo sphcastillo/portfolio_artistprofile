de04d74afef22a31a58e43c7bd8fa5b005d2284a
Sophia Castillo <sphcastillo@gmail.com>
2024-03-06 20:53:55 -0800

    paragraph altered to ABOUT

--- components/About.tsx
+++ components/About.tsx
@@ -32,12 +32,11 @@ function About() {
                 <Image src={girlinred} alt="girl in red" className="rounded-full md:rounded-lg object-cover"/>
             </motion.div>
 
-            <div className="space-y-10 px-0 md:px-10 text-text-[#ddd4c6]">
-                <h4 className="text-4xl font-semibold"> WHERE IT{" "}
+            <div className="space-y-10 px-0 md:px-10 text-text-[#ddd4c6] pt-2">
+                <h4 className="text-3xl font-semibold"> WHERE IT{" "}
                     <span className="decoration-text-[#ddd4c6]/50 underline">ALL</span>{" "}STARTED
                 </h4>
-                <p className="text-lg">Marie Ulven Ringheim was born in Horten, Norway on February 16, 1999. She grew up in the small town — which she described as “quiet and kinda boring” — with her sisters and divorced parents. She didn’t have any instruments, even though her grandfather could play the guitar and piano. Marie never really had an interest in music; her true passion at the time was fingerboarding, a sport where a person “rides” a small working replica of a skateboard and maneuvers it with their fingers. You can even find videos of her competing in some fingerboarding battles on YouTube.</p>
-                <p className="text-lg">Her grandfather gave Marie a guitar as a Christmas gift in 2012, though she didn’t start playing it until she lost interest in fingerboarding in 2013. She then learned guitar, piano, and music production from the comfort of her own bedroom. Marie first released Norwegian music on Soundcloud under the moniker of “Lydia X”. She then coined the stage name “girl in red” after her crush looked really pretty in a red sweater.</p>
+                <p className="text-lg">Marie Ulven Ringheim was born in Horten, Norway on February 16, 1999. She grew up in the small town — which she described as “quiet and kinda boring” — with her sisters and divorced parents. Marie never really had an interest in music; her true passion at the time was fingerboarding, a sport where a person “rides” a small working replica of a skateboard and maneuvers it with their fingers. You can even find videos of her competing in some fingerboarding battles on YouTube. Marie first released Norwegian music on Soundcloud under the moniker of “Lydia X”. She then coined the stage name “girl in red” after her crush looked really pretty in a red sweater.</p>
             </div>
         </motion.div>
     </div>
