8aadaa3d96c22a4256dfa53958482b7e1e39fbc0
Sophia Castillo <sphcastillo@gmail.com>
2024-03-25 22:40:20 -0700

    Changes to next-auth.d.ts after Vercel error

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,7 +3,8 @@ import NextAuth, { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
     interface Session {
         user: {
-            id?: string;
+            name?: string;
+            image?: string;
         }  & DefaultSession['user'];
     }
 }
