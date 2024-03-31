8dcc044f1754513df0cd629a1f8b80e6c01c1815
Sophia Castillo <sphcastillo@gmail.com>
2024-03-30 23:51:01 -0700

    Mobile screen CSS

--- components/EmbedArtist.tsx
+++ components/EmbedArtist.tsx
@@ -4,7 +4,7 @@ function EmbedArtist() {
   return (
     <div className="w-full flex justify-center pb-[65px]">
         <iframe 
-            className="w-9/10 sm:w-3/5"
+            className="w-4/5 sm:w-3/5"
             src="https://open.spotify.com/embed/artist/3uwAm6vQy7kWPS2bciKWx9?utm_source=generator" 
             width="100%" 
             height="352" 
